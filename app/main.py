4208025c21594e23ba6e676d62bd3b1bbe914fb3
Santiago Jaramillo <san_jara@outlook.com>
2021-10-01 15:00:46 -0500

    add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model = joblib.load("app/model.joblib")
 
